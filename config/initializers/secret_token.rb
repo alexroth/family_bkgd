91f8cd786c7441c9a6c76d386661adfffaf3b657
Alex Roth <alexroth2001@yahoo.com>
2013-12-06 10:03:39 -0500

    Initial commit for family_bkgd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FamilyBkgd::Application.config.secret_key_base = '9fb144e85fd0d314d724ad0724d7a8deeb2e376a90dd93ee6224e19e681346959018639117f539c1cbdb5533bbfd5a553f98ade70a1deedd094497c17f7f2483'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FamilyBkgd::Application.config.session_store :cookie_store, key: '_family_bkgd_session'
